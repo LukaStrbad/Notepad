45e4c00b4e1ef1a3d88d391f53c60f83b23dea21
Luka1308 <s.luka.1308@gmail.com>
2020-02-26 21:26:53 +0100

    added replace functionality

--- NotepadCore/Find.xaml.cs
+++ NotepadCore/Find.xaml.cs
@@ -20,16 +20,39 @@ namespace NotepadCore
             InitializeComponent();
             FindTextBox.Focus();
             _mw = Application.Current.Windows[0] as MainWindow;
+
+            FindTextBox.TextChanged += (sender, args) => RecalculateNextMatch();
+            RegExCheckBox.Checked += (sender, args) => RecalculateNextMatch();
+            RegExCheckBox.Unchecked += (sender, args) => RecalculateNextMatch();
+            CaseSensitiveCheckBox.Checked += (sender, args) => RecalculateNextMatch();
+            CaseSensitiveCheckBox.Unchecked += (sender, args) => RecalculateNextMatch();
         }
 
         private Regex FindRegex
         {
             get
             {
-                if (RegExCheckBox.IsChecked ?? false)
-                    return new Regex(FindTextBox.Text);
-                return new Regex(Regex.Escape(FindTextBox.Text));
+                bool caseSensitive = CaseSensitiveCheckBox.IsChecked ?? false;
+
+                try
+                {
+                    if (RegExCheckBox.IsChecked ?? false)
+                        return new Regex(FindTextBox.Text, caseSensitive ? RegexOptions.None : RegexOptions.IgnoreCase);
+                }
+                catch
+                {
+                    // ignored
+                }
+
+                return new Regex(Regex.Escape(FindTextBox.Text),
+                    caseSensitive ? RegexOptions.None : RegexOptions.IgnoreCase);
             }
+        }
+
+        private void RecalculateNextMatch()
+        {
+            var textRange = new TextRange(TextBox.Document.ContentStart, TextBox.Document.ContentEnd);
+            _currentMatch = FindRegex.Match(textRange.Text);
         }
 
         private RichTextBox TextBox => ((TextEditor) _mw.Tabs.SelectedContent).MainTextBox;
@@ -41,22 +64,33 @@ namespace NotepadCore
             FindText();
         }
 
+        private void SetNextMatch()
+        {
+            var textRange = new TextRange(TextBox.Document.ContentStart, TextBox.Document.ContentEnd);
+
+            _currentMatch = _currentMatch?.NextMatch();
+            if (_currentMatch == null || !_currentMatch.Success)
+                _currentMatch = FindRegex.Match(textRange.Text);
+        }
+
         private void FindText()
         {
             var textRange = new TextRange(TextBox.Document.ContentStart, TextBox.Document.ContentEnd);
 
-            if (_currentMatch == null || !_currentMatch.Success)
-                _currentMatch = FindRegex.Match(textRange.Text);
             int lineNumber = 0;
+            // Calculate line number of current match
             for (; lineNumber < TextBox.Document.Blocks.Count; lineNumber++)
+            {
                 if (new TextRange(TextBox.Document.Blocks.ElementAt(lineNumber).ContentStart,
                         TextBox.Document.Blocks.ElementAt(lineNumber).ContentEnd)
                     .Contains(TextEditor.GetTextPointAt(textRange.Start, _currentMatch.Index)))
                     break;
+            }
+
             TextBox.Selection.Select(TextEditor.GetTextPointAt(textRange.Start, _currentMatch.Index - lineNumber * 2),
                 TextEditor.GetTextPointAt(textRange.Start,
                     _currentMatch.Index + _currentMatch.Length - lineNumber * 2));
-            _currentMatch = _currentMatch.NextMatch();
+            SetNextMatch();
 
             _mw.Focus();
             Focus();
@@ -64,9 +98,10 @@ namespace NotepadCore
 
         private void ReplaceButton_Click(object sender, RoutedEventArgs e)
         {
-            //textBox.SelectedText = ReplaceTextBox.Text ?? "";
-
-            //FindText(true, false);
+            if (_currentMatch == null || !_currentMatch.Success)
+                SetNextMatch();
+            TextBox.Selection.Text = FindRegex.Replace(TextBox.Selection.Text, ReplaceTextBox.Text);
+            FindText();
         }
 
         private void FindTextBox_TextChanged(object sender, TextChangedEventArgs e)
