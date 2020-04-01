dd42021aa2c7696ca6f53d168f9f89edbc7d8b84
LukaStrbad <s.luka.1308@gmail.com>
2020-04-01 18:29:06 +0200

    Removed unnecessary code

--- NotepadCore/FontWindow.xaml.cs
+++ NotepadCore/FontWindow.xaml.cs
@@ -18,15 +18,14 @@ namespace NotepadCore
             var userSettings = Settings.UserSettings.Create();
 
             // write font sizes from 8 to 96
-            for (var i = 8; i <= 96; i++) FontSizeChooseListBox.Items.Add(i.ToString());
+            for (var i = 8; i <= 96; i++) FontSizeChooseListBox.Items.Add(i);
 
             FontChooseListBox.SelectedItem = new System.Windows.Media.FontFamily(userSettings.EditorFontFamily); // selects the fontfamily in the listbox
             FontSizeChooseListBox.SelectedItem =
-                userSettings.EditorFontSize.ToString(); // selects the font size in the listbox
+                userSettings.EditorFontSize; // selects the font size in the listbox
 
             FontChooseListBox.ScrollIntoView(new System.Windows.Media.FontFamily(userSettings.EditorFontFamily)); // scrolls to the font in the listbox
-            FontSizeChooseListBox.ScrollIntoView(userSettings.EditorFontSize
-                .ToString()); // scrolls to the font size in the listbox
+            FontSizeChooseListBox.ScrollIntoView(userSettings.EditorFontSize); // scrolls to the font size in the listbox
         }
 
         private void FontOKButton_Click(object sender, RoutedEventArgs e)
